339238fc3f4bad7ecf8367decb37182a36b72f62
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-06-27 15:58:57 +0300

    Add CQRS interfaces

---
+++ Shared/SharedKernel/Messaging/IQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace SharedKernel.Messaging;
+public interface IQuery<TResponse>;
+
---
+++ Shared/SharedKernel/Messaging/IQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿using SharedKernel.Base;
+
+namespace SharedKernel.Messaging;
+public interface IQueryHandler<in TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+{
+    Task<Result<TResponse>> Handle(TQuery query, CancellationToken cancellationToken);
+}
+
