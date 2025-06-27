12ae345e1c741c724f610e3c151c3138d58558ba
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-06-27 15:49:34 +0300

    Add OCR service provider contract

---
+++ OCRService/OCRService.Domain/Interfaces/IOCRServiceProvider.cs
@@ -0,0 +1,11 @@
+﻿using OCRService.Domain.Entities.Input;
+using OCRService.Domain.Entities.Output;
+using SharedKernel;
+
+namespace OCRService.Domain.Interfaces;
+
+public interface IOCRServiceProvider
+{
+    public Task<bool> IsAvailable();
+    public Task<Result<OCROutput>> ProcessOCR (OCRInput ocrInput);
+}
