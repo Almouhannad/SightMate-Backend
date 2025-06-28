e26f075ae76cc5da8a2bbcd583843e332831a540
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-06-28 23:15:44 +0300

    Move Language to shared kernel

--- OCRService/OCRService.Domain/Entities/Input/Options/Language/OCRLanguages.cs
+++ OCRService/OCRService.Domain/Entities/Input/Options/Language/OCRLanguages.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections.ObjectModel;
+using LanguageClass = SharedKernel.Base.Language;
 
 namespace OCRService.Domain.Entities.Input.Options.Language;
 
 public static class OCRLanguages
 {
-    private static readonly OCRLanguage _english = new(name: "English", code: "en");
-    public static OCRLanguage ENGLISH => _english;
+    private static readonly LanguageClass _english = new(name: "English", code: "en");
+    public static LanguageClass ENGLISH => _english;
 
-    private static readonly OCRLanguage _arabic = new(name: "Arabic", code: "ar");
-    public static OCRLanguage Arabic => _arabic;
+    private static readonly LanguageClass _arabic = new(name: "Arabic", code: "ar");
+    public static LanguageClass Arabic => _arabic;
 
-    public static readonly ReadOnlyDictionary<string, OCRLanguage> OCRSupportedLanguagesMap =
+    public static readonly ReadOnlyDictionary<string, LanguageClass> OCRSupportedLanguagesMap =
         new(
-            new Dictionary<string, OCRLanguage>(StringComparer.OrdinalIgnoreCase)
+            new Dictionary<string, LanguageClass>(StringComparer.OrdinalIgnoreCase)
             {
                     { _english.Code, _english },
                     { _arabic.Code, _arabic }
--- OCRService/OCRService.Domain/Entities/Input/Options/OCROptions.cs
+++ OCRService/OCRService.Domain/Entities/Input/Options/OCROptions.cs
@@ -1,11 +1,12 @@
 ﻿using OCRService.Domain.Entities.Input.Options.Language;
 using OCRService.Domain.Entities.Input.Options.Model;
+using LanguageClass = SharedKernel.Base.Language;
 
 namespace OCRService.Domain.Entities.Input.Options;
 
 public class OCROptions
 {
-    public OCRLanguage Language { get; set; } = OCRLanguages.ENGLISH;
+    public LanguageClass Language { get; set; } = OCRLanguages.ENGLISH;
     public String Model { get; set; } = OCRModels.GEMMA;
     public bool Describe { get; set; } = true;
 
