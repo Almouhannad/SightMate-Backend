cc78d177afcc7ba2f52dbf23f1a2855359b7622a
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-07-03 23:16:48 +0300

    Add models/endpoints extensions

---
+++ IdentityService/IdentityService.API/Models/RegisterModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService.API.Models;
+public class RegisterModel
+{
+    [Required]
+    public String Email { get; set; } = String.Empty;
+    [Required]
+    public required String Password { get; set; } = String.Empty;
+
+}
