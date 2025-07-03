4797fe6dc861c14ce917239cbd365dffb8c27ffe
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-07-04 01:38:12 +0300

    Add domain

---
+++ IdentityService/IdentityService.Domain/Entities/Roles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace IdentityService.Domain.Entities;
+
+public static class Roles
+{
+    private static readonly Role _user = new() { Name = "USER" };
+    public static Role USER => _user;
+
+    private static readonly Role _admin = new() { Name = "ADMIN" };
+    public static Role ADMIN => _admin;
+
+    public static readonly ReadOnlyDictionary<string, Role> ROLES =
+        new(
+            new Dictionary<string, Role>(StringComparer.OrdinalIgnoreCase)
+            {
+                    { _user.Name, USER },
+                    { _admin.Name, ADMIN }
+            }
+        );
+}
---
+++ IdentityService/IdentityService.Domain/Interfaces/IUserManager.cs
@@ -0,0 +1,15 @@
+﻿using IdentityService.Domain.Entities;
+using SharedKernel.Base;
+
+namespace IdentityService.Domain.Interfaces;
+
+public interface IUserManager
+{
+    public Task<Result<User>> Create(String firstName, String lastName, String email, String password, List<Role>? roles);
+
+    public Task<Result<User>> FindByEmail(String email);
+    public Task<Result<bool>> CheckPassword(User user, String password);
+    public Task<Result> AddRole(User user, Role role);
+
+
+}
