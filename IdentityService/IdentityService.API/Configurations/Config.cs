f5daef1ca6541e113dad4b0690d92a4a1f21177b
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-07-03 19:13:38 +0300

    Register db and authentication

---
+++ IdentityService/IdentityService.API/Configurations/Config.cs
@@ -0,0 +1,14 @@
+﻿using SharedKernel.Config;
+
+namespace IdentityService.API.Configurations;
+
+public static class CONFIG
+{
+    // Env-var names
+    private const string ConnectionStringVar = "CONNECTION_STRING";
+
+    // Lazy, exception-safe properties
+    private static readonly Lazy<String> _connectionString = new(() => ENVHelper.GetEnv(ConnectionStringVar));
+    public static String ConnectionString => _connectionString.Value;
+
+}
--- IdentityService/IdentityService.API/Program.cs
+++ IdentityService/IdentityService.API/Program.cs
@@ -1,8 +1,28 @@
+using IdentityService.API.Configurations;
+using IdentityService.API.Database;
+using IdentityService.API.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication()
+    .AddBearerToken(IdentityConstants.BearerScheme);
+
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddApiEndpoints();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(CONFIG.ConnectionString);
+});
 
 var app = builder.Build();
 
@@ -14,6 +34,8 @@ if (app.Environment.IsDevelopment())
     {
         options.SwaggerEndpoint("/openapi/v1.json", "OpenAPI V1");
     });
+
+    app.ApplyMigrations();
 }
 
 app.MapGet("/health", () =>
@@ -21,5 +43,14 @@ app.MapGet("/health", () =>
     return new { Status = "OK" };
 })
 .WithName("Health");
+
+app.MapGet("/users/me", async(ClaimsPrincipal claims, ApplicationDbContext context) =>
+{
+    String userId = claims.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+    return await context.Users.FindAsync(Guid.Parse(userId));
+
+}).RequireAuthorization();
+
+app.MapIdentityApi<User>();
 
 app.Run();
