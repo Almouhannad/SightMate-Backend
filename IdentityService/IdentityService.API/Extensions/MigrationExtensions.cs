ae23b8d7291145e1e894043a5b5caad136536523
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-07-03 19:13:21 +0300

    initial migration

---
+++ IdentityService/IdentityService.API/Extensions/MigrationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using IdentityService.API.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.API.Extensions;
+
+public static class MigrationExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+        using ApplicationDbContext context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        context.Database.Migrate();
+    }
+
+}
