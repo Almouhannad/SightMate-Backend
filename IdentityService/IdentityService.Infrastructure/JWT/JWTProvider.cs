bc8e6d9776586e8d6a8985cb03573305a7902ac7
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-07-04 03:17:21 +0300

    Implement JWT provider

---
+++ IdentityService/IdentityService.Infrastructure/JWT/JWTProvider.cs
@@ -0,0 +1,38 @@
+﻿using IdentityService.Config;
+using IdentityService.Domain.Entities;
+using IdentityService.Domain.Interfaces;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace IdentityService.Infrastructure.JWT;
+
+public class JWTProvider : IJWTProvider
+{
+    public string Create(User user)
+    {
+        string secretKey = CONFIG.JWTSecretKey;
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(
+            [
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email)
+            ]),
+            SigningCredentials = credentials,
+            Issuer = CONFIG.JWTIssuer,
+            Audience = CONFIG.JWTAudience,
+        };
+
+        var handler = new JsonWebTokenHandler();
+
+        string token = handler.CreateToken(tokenDescriptor);
+
+        return token;
+    }
+}
