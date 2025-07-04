2d270698df6a287092f66c07b2a3f602647a3f68
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-07-04 13:10:31 +0300

    Add login

---
+++ IdentityService/IdentityService.Application/Login/LoginQuery.cs
@@ -0,0 +1,5 @@
+﻿using SharedKernel.Messaging;
+
+namespace IdentityService.Application.Login;
+
+public sealed record LoginQuery (String Email, String Password) : IQuery<LoginQueryResponse>;
---
+++ IdentityService/IdentityService.Application/Login/LoginQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace IdentityService.Application.Login;
+
+public class LoginQueryValidator : AbstractValidator<LoginQuery>
+{
+    public LoginQueryValidator()
+    {
+        RuleFor(c => c.Email).NotEmpty().EmailAddress();
+        RuleFor(c => c.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+            .Matches(@"\d").WithMessage("Password must contain at least one digit.")
+            .Matches(@"[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
+    }
+}
+}
