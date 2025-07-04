ed829acb9d87b835d458fd1142b3c5bc92201b29
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-07-04 03:18:06 +0300

    Register DI

--- IdentityService/IdentityService.API/Program.cs
+++ IdentityService/IdentityService.API/Program.cs
@@ -1,41 +1,13 @@
+using IdentityService.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddInfrastructure();
+
+
 builder.Services.AddOpenApi();
-
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//{
-//    options.UseNpgsql(CONFIG.ConnectionString);
-//});
-
-//builder.Services.AddIdentity<User, Role>()
-//    .AddEntityFrameworkStores<ApplicationDbContext>()
-//    .AddDefaultTokenProviders();
-
-
-//builder.Services.AddAuthentication(options =>
-//{
-//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-//    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-//    //options.DefaultSignInScheme = IdentityConstants.ApplicationScheme;
-//})
-//.AddJwtBearer(options =>
-//{
-//    options.TokenValidationParameters = new TokenValidationParameters
-//    {
-//        ValidateIssuer = true,
-//        ValidateAudience = true,
-//        ValidateLifetime = true,
-//        ValidateIssuerSigningKey = true,
-//        ValidIssuer = CONFIG.JWTIssuer,
-//        ValidAudience = CONFIG.JWTAudience,
-//        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(CONFIG.JWTSecretKey))
-//    };
-//});
-
-//builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -48,7 +20,7 @@ if (app.Environment.IsDevelopment())
         options.SwaggerEndpoint("/openapi/v1.json", "OpenAPI V1");
     });
 
-    //app.ApplyMigrations();
+    app.ApplyMigrations();
 }
 
 app.MapGet("/health", () =>
